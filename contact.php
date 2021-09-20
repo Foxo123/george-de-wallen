84da23a461ca6943827acc91289ac8b3fa8477f9
Kapello <330990@student.mboutrecht.nl>
2021-09-20 15:13:58 +0200

    Update contact.php
    I wrote a bit of About Us text and copied  the bit from the official George website

--- contact.php
+++ contact.php
@@ -7,6 +7,37 @@
     <title>Document</title>
 </head>
 <body>
-    
+
+ABOUT US
+
+Our story began in 1969 when the Founder of George Restaurants & Co. had an idea for amazing food served with a smile.
+He was tired of going to restaurants that didn't acknowledge him. So he set out to create one with top-notch service for anyone.
+In 1973, he had already set up 13 restaurants under this motto and it was a big success! 6 years later he had published 
+this formula for franchise-owners alike. Hence our story began in de Wallen in 1989 and we have been serving happy customers ever since.
+
+OUR MISSION
+
+We strive towards excellence in food and service, to create the fond memories our customers treasure. 
+Share a celebration or anniversary with your family, friends, and loved ones.
+Our chefs are some of the best on the market and we make sure to price our products competitively. As George has intended it to.
+Our patented formula makes for a great night out, no matter the circumstance!
+
+
+
+From that local neighbourhood spot where everybody knows your name to a light-filled, eclectic restaurant with view
+ on the Amstel river: George has it all.
+
+Serving you iconic dishes and timeless classics with New York, Paris, the Côte d'Azur and Hong Kong in mind,
+ along with local ingredients, freshly roasted coffee and signature cocktails, our restaurants are open all day,
+  every day –  so come join us for an early morning espresso, a lazy Sunday brunch or an intimate dinner with friends
+   in a relaxed, elegant atmosphere.
+
+A FEAST FOR YOUR SENSES
+Most of our restaurants are designed by Amsterdam based Architecture & Design studio Framework, 
+who signed for our bespoke interiors and sophisticated yet intimate ambiance,
+featuring typical George elements like metro tiles, lush leather couches, brass lighting, and marble bars.
+With each location having its own charm, your visit to George quite literally is a feast for your eyes and taste buds.
+
+Most of our locations have stunning, sunny terraces and are open all day, every day for breakfast, lunch and dinner.
 </body>
 </html>
