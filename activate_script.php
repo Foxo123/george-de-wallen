3e3a926acbc658106d21cf65fe3584c7a1c50bd1
Tom Lengkeek <330713@student.mboutrecht.nl>
2021-09-20 14:34:40 +0200

    initial commit
    the initial commit

--- activate_script.php
+++ activate_script.php
@@ -2,18 +2,23 @@
   include("./connect_db.php");
   include("./functions.php");
 
-  $id = sanitize($_POST["id"]);
+  $em = sanitize($_POST["em"]);
   $pwh = sanitize($_POST["pwh"]);
   $password = sanitize($_POST["password"]);
   $passwordCheck = sanitize($_POST["passwordCheck"]);
+  $firstN = sanitize($_POST["LastName"]);
+  $infix = sanitize($_POST["Infix"]); 
+  $lastN = sanitize($_POST["FirstName"]);
+  $phoneN = sanitize($_POS["PhoneNumber"]);
+
   
   if (empty($_POST["password"]) || empty($_POST["passwordCheck"])) {
-    header("Location: ./index.php?content=message&alert=password-empty&id=$id&pwh=$pwh");
+    header("Location: ./index.php?content=message&alert=password-empty&em=$em&pwh=$pwh");
   } elseif (strcmp($password, $passwordCheck)) {
-    header("Location: ./index.php?content=message&alert=nomatch-password&id=$id&pwh=$pwh");
+    header("Location: ./index.php?content=message&alert=nomatch-password&em=$em&pwh=$pwh");
   } else {
     
-    $sql = "SELECT * FROM `register` WHERE `id` = $id";
+    $sql = "SELECT * FROM `password` WHERE `email` = '$em'";
 
     $result = mysqli_query($conn, $sql);
 
@@ -25,23 +30,24 @@
         header("Location: ./index.php?content=message&alert=already-active");
       } else {
 
-        if ( !strcmp($record["password"], $pwh)) {
+        if ( !strcmp($record["passwd"], $pwh)) {
           // 1. Maak een passwordhash voor het nieuw gekozen wachtwoord
           $password_hash = password_hash($password, PASSWORD_BCRYPT);
     
           // 2. Ga het record updaten met het nieuw gekozen gehashte wachtwoord
-          $sql = "UPDATE `register` 
-                  SET    `password` = '$password_hash',
-                         `activated`= 1 
-                  WHERE  `id` = $id
-                  AND    `password` = '$pwh'";
+          $sql = "UPDATE `password` 
+                  SET    `passwd` = '$password_hash',
+                         `updatedAt`= CURRENT_TIMESTAMP,
+                         `activated` = 1
+                  WHERE  `email` = '$em'
+                  AND    `passwd` = '$pwh'";
     
           if (mysqli_query($conn, $sql)) {
             // succes
             header("Location: ./index.php?content=message&alert=update-success");
           } else {
             // error
-            header("Location: ./index.php?content=message&alert=update-error&id=$id&pwh=$pwh");        
+            header("Location: ./index.php?content=message&alert=update-error&em=$em&pwh=$pwh");        
           }
         } else {
           header("Location: ./index.php?content=message&alert=no-match-pwh");        
