8dbed1192ee2e3128913774dc0f73e067d88c392
LuucStigter <stigterluuc@gmail.com>
2021-09-21 16:13:39 +0200

    added pictures to carousel
    added pictures to carousel

--- about-us.php
+++ about-us.php
@@ -60,13 +60,13 @@
           </ol>
           <div class="carousel-inner">
             <div class="carousel-item active">
-              <img class="d-block w-100" src="./img/about-us-carousel/gray.png" alt="First slide">
+              <img class="d-block w-100" src="./img/about-us-carousel/food1.jpg" alt="First slide">
             </div>
             <div class="carousel-item">
-              <img class="d-block w-100" src="./img/about-us-carousel/gray.png" alt="Second slide">
+              <img class="d-block w-100" src="./img/about-us-carousel/food2.jpg" alt="Second slide">
             </div>
             <div class="carousel-item">
-              <img class="d-block w-100" src="./img/about-us-carousel/gray.png" alt="Third slide">
+              <img class="d-block w-100" src="./img/about-us-carousel/food3.jpg" alt="Third slide">
             </div>
           </div>
           <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
