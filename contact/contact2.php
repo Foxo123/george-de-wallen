4a7af77619db2802ee30c8d3b3222c6894747b54
Kapello <330990@student.mboutrecht.nl>
2021-09-28 16:49:43 +0200

    Changed some pictures and removed OOP assignments
    Also added a test page; contact.php

---
+++ contact/contact2.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
+    <link id="u-page-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Merriweather:300,300i,400,400i,700,700i,900,900i">
+    <?php include("./transition.php"); ?>
+    <title>Hello, world!</title>
+
+</head>
+
+<body>
+    <h1>Hello, world!</h1>
+    !Luuc: <div class="container">
+  <div class="row">
+    <div class="col-4" id="img1">
+    <img src=".\contact\images\coffee.png" alt="">
+    </div>
+    <div class="col-4" id="maps">
+      google maps hier
+    </div>
+    <div class="col-4" id="img2">
+      hier img 2
+    </div>
+  </div>
+</div>
+
+
+    <style>
+ #img1 {
+  width: 250px;
+  height: 250px;
+}
+
+#img2 {
+  width: 250px;
+  height: 250px;
+} 
+</style>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="./kkstyle.css">
+
+</body>
+
+</html>
