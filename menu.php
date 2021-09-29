7eb4b406d028ad49e105964a33faa117f719035a
Mahircan Alkan <328827@student.mboutrecht.nl>
2021-09-29 12:41:03 +0200

    page fin 1.0

--- menu.php
+++ menu.php
@@ -13,18 +13,18 @@
   <div class="box" id="momi2">
   	<img src="./img/Kadefoto1.jpg">
 	  	<a href="#menu"> 
-		   <span>Menu</span>
+		   <span>Menu</span>  
 		</a>
   </div>
   <div class="box" id="momi1">
- 	<img src="./img/Specialties1.jpg">
-	 <a href="#special"> 
+ 	<img src="./img/Specialties1.jpg" >
+	 	<a href="#special"> 
 		   <span>Specialties</span>
 		</a>
   </div>
   <div class="box" id="momi3">
     <img src="./img/Health1.jpg">
-	<a href="#allergy"> 
+		<a href="#allergy"> 
 		   <span>Allergy info</span>
 		</a>
   </div>
@@ -32,21 +32,22 @@
 
 
 <div class="afstand" style="margin: 0px 125px;">
-<h1> <center style="text-decoration: underline; padding-top: 50px; font-family:Apple Chancery; font-size:50px;">Discover more taste!</center> </h1>
+<h1> <center id="menu" style="text-decoration: underline; padding-top: 50px; font-family:Apple Chancery; font-size:50px;">Discover more taste!</center> </h1>
 
 
-<div class="row" style="padding-top: 175px;" id="menu">
+<div class="row" style="padding-top: 175px;">
     <div class="col-6" style="border-right-style: solid;">
-        <center style="font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;;font-size:50px; border-style: groove;">Our sublime menu!</center>
-
-        <center style="font-family:Georgia, sans-serif; font-size: 25px; padding-top: 25px;">Chick'n Tartaire speciale</center>
-        <center style="font-family:Georgia, sans-serif; font-size: 25px;">George´s Corn bread with Crème fraîche</center>
-        <center style="font-family:Georgia, sans-serif; font-size: 25px;">Tender Beef with Garlic and Tomato sauce</center>
-        <center style="font-family:Georgia, sans-serif; font-size: 25px;">Spareribs with extra Honey and Mustard</center>
-        <center style="font-family:Georgia, sans-serif; font-size: 25px;">Fish soup with 4 diffent fish, all caught in Amsterdam</center>
-        <center style="font-family:Georgia, sans-serif; font-size: 25px;">Rice with Onions and lamb dipped in Honey</center>
-        <center style="font-family:Georgia, sans-serif; font-size: 25px;">Hen meat with sliced Potatoes and Cucumber</center>
-		<center style="font-family:Georgia, sans-serif; font-size: 25px;">Tart chicken meat with rosato´s and chips</center>
+		<center style="font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;;font-size:50px; border-style: groove;">Regulare Menu</center>
+		
+		<center style="font: italic 1.2em Fira Sans, serif; font-size: 25px; padding-top: 25px">Pork chips with regular Chips................€12,95</center>
+		<center style="font: italic 1.2em Fira Sans, serif; font-size: 25px;">Canadian Salmon with Sliced vegetables................€15,95</center>
+		<center style="font: italic 1.2em Fira Sans, serif; font-size: 25px;">Chicken sliced Oregano................€21,95</center>
+		<center style="font: italic 1.2em Fira Sans, serif; font-size: 25px;">Meat with Garlic and sliced cucumber steak................€18,95</center>
+		<center style="font: italic 1.2em Fira Sans, serif; font-size: 25px;">Tapenade with Cocos and mix of 5 different sauces................€18.95</center>
+		<center style="font: italic 1.2em Fira Sans, serif; font-size: 25px;">Pomenade with sliced chicken and carrots................€14,95</center>
+		<center style="font: italic 1.2em Fira Sans, serif; font-size: 25px;">The regular Kapsalon!................€12.95</center>
+		<center style="font: italic 1.2em Fira Sans, serif; font-size: 25px;">Crab with Fish rings................€22,95</center>
+		<center style="font: italic 1.2em Fira Sans, serif; font-size: 25px;">Mr baker eggs with sliced onion................€12,95</center>
 </div>
     <div class="col-6">
         <img src="./img/rechts1.jpg" class="w-75 h-100" style=" width: 40%;  margin: auto; width: 50%;border: 3px solid black;padding: 10px;">
@@ -58,16 +59,17 @@
 </div>
     
     <div class="col-6" style="padding-top: 50px;">
-        <center style="font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;;font-size:50px; border-style: groove;">Our normale grande!</center>
-
-        <center style="font-family:Georgia, sans-serif; font-size: 25px; padding-top: 25px;">Chick'n Tartaire speciale</center>
-        <center style="font-family:Georgia, sans-serif; font-size: 25px;">George´s Corn bread with Crème fraîche</center>
-        <center style="font-family:Georgia, sans-serif; font-size: 25px;">Tender Beef with Garlic and Tomato sauce</center>
-        <center style="font-family:Georgia, sans-serif; font-size: 25px;">Spareribs with extra Honey and Mustard</center>
-        <center style="font-family:Georgia, sans-serif; font-size: 25px;">Fish soup with 4 diffent fish, all caught in Amsterdam</center>
-        <center style="font-family:Georgia, sans-serif; font-size: 25px;">Rice with Onions and lamb dipped in Honey</center>
-        <center style="font-family:Georgia, sans-serif; font-size: 25px;">Hen meat with sliced Potatoes and Cucumber</center>
-		<center style="font-family:Georgia, sans-serif; font-size: 25px;">Tart chicken meat with rosato´s and chips</center>
+		<center style="font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;;font-size:50px; border-style: groove;">Traditionale!</center>
+		
+		<center style="font: italic 1.2em Fira Sans, serif; font-size: 25px; padding-top: 25px">Pork chips with regular Chips................€12,95</center>
+		<center style="font: italic 1.2em Fira Sans, serif; font-size: 25px;">Canadian Salmon with Sliced vegetables................€15,95</center>
+		<center style="font: italic 1.2em Fira Sans, serif; font-size: 25px;">Chicken sliced Oregano................€21,95</center>
+		<center style="font: italic 1.2em Fira Sans, serif; font-size: 25px;">Meat with Garlic and sliced cucumber steak................€18,95</center>
+		<center style="font: italic 1.2em Fira Sans, serif; font-size: 25px;">Tapenade with Cocos and mix of 5 different sauces................€18.95</center>
+		<center style="font: italic 1.2em Fira Sans, serif; font-size: 25px;">Pomenade with sliced chicken and carrots................€14,95</center>
+		<center style="font: italic 1.2em Fira Sans, serif; font-size: 25px;">The regular Kapsalon!................€12.95</center>
+		<center style="font: italic 1.2em Fira Sans, serif; font-size: 25px;">Crab with Fish rings................€22,95</center>
+		<center style="font: italic 1.2em Fira Sans, serif; font-size: 25px;">Mr baker eggs with sliced onion................€12,95</center>
     </div>
 </div>
 
@@ -79,10 +81,10 @@
 </blockquote>
 
 
-<h1> <center style="text-decoration: underline; padding-top: 50px; font-family:Apple Chancery; font-size:50px;">Specialties!</center> </h1>
+<h1> <center id="special" style="text-decoration: underline; padding-top: 50px; font-family:Apple Chancery; font-size:50px;">Specialties!</center> </h1>
 
 
-<div class="row" style="padding-top: 150px;" id="special">
+<div class="row" style="padding-top: 150px;">
     <div class="col-6" style="border-right-style: solid;">
         <center style="font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;;font-size:50px; border-style: groove;">Speciale Formaggio!</center>
 
@@ -126,35 +128,36 @@
 
 
 </div>
-<div style="display:flex; justify-content:center; padding-top: 50px"> 
-	<img src="./img/georgelogo1.jpg" alt="">
-</div>
+	<div style="display:flex; justify-content:center;">
+		<img src="./img/georgelogo1.jpg" alt="">
+	</div>
 
 
 
 <div class="afstand" style="margin: 0px 100px;" id="allergy">
 <div class="jumbotron"  id="jumbo1" style="text-align: center; font-family:Arial; padding-top: 200px;">
-  <h1 class="display-4" >Food info/Allergies!</h1>
-  <p class="lead"></p>
-  <hr class="my-4">
-  <p>Our food products contain: 
-	  gluten,
-	  sugar/salt,
-	  oil,
-	  meat,
-	  bio meat,
-	  milk,
-	  eggs,
-	  and other variaty off products.
-  </p>
+  	<h1 class="display-4" >Food info/Allergies!</h1>
+  		<p class="lead"></p>
+  			<hr class="my-4">
+  				<p>Our food products contain: 
+	 			 gluten,
+	 			 sugar/salt,
+	  			oil,
+	 			 meat,
+	 			 bio meat,
+				  milk,
+	 			 eggs,
+				  and other variaty off products.
+ 		 </p>
   <p style="text-decoration: underline;"> 
 	  Please if you have any allergies then contact our restaurant BEFORE you reserve and wait for further information!	
   </p>
-  <a class="btn btn-primary btn-lg"  href="#" role="button">CALL US!</a>
-</div>
-</div>
+  	<a class="btn btn-primary btn-lg"  href="#" role="button">CALL US!</a>
+		</div>
+	</div>
 
 </div>
+
 
 <style>
 .footer {
