0f5c1f03b7d5ef0915f26a2152e0190d24210592
Tom Lengkeek <330713@student.mboutrecht.nl>
2021-09-30 08:49:19 +0200

    removed footer styling
    -

--- menu.php
+++ menu.php
@@ -160,17 +160,5 @@
 </div>
 
 
-<style>
-.footer {
-  position: fixed;
-  left: 0;
-  bottom: 0;
-  width: 100%;
-  background-color: black;
-  color: white;
-  text-align: center;
-  font: arial;
-}
-</style>
 
 
