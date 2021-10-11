dc984a0d47a786ebcf51e05ac0bcf9af6089f7a1
tjardo de heer <330181@student.mboutrecht.nl>
2021-10-11 14:43:22 +0200

    Working on the corona page.
    Adding padding + content to the page.

---
+++ Corona.php
@@ -0,0 +1,82 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+ </head>
+  <body>
+  <div class="container" style= "margin-top: 30;">
+  <div class="row row-cols-2">
+    <div class="col" style= " border-right-style: solid;">>
+    <h2 style="margin-top:60 , border-right-style: solid" class=><center>Onze coronaregels bij Resturant George de wallen</h2></center>
+                      <p class="u-text u-text-4"><center><br>Sinds de corona crisis moeten wij aan een aantal regels voordoen. 
+                      <br>Dit is om veiligheid voor u maar ook voor onze werknemers.</br>
+                      <br>Bij vragen contacteer u ons:</br>
+                      <center><button type="button" class="btn btn-primary btn-lg" style="margin-top: 30;">Klik hier</button></center></div>
+    <div class="col">
+      <img src="./img/corona.png" alt="" class="img-fluid w-80"></div>
+  </div>
+
+  <div class="container" style= "margin-top: 30;">
+  <div class="row row-cols-2" style="padding-bottom: 15px;">
+    <div class="col" style= " border-right-style: solid;">
+    <img src="./img/corona2.png" alt="" class="img-fluid w-80"></div>
+    <div class="col">
+    <h2 style="margin-top:100" class=><center>Kijk hier onder voor onze regels:</h2>
+    <p class="u-text u-text-4"><center><br>Onze werknemers doen hard hun best om zo schoon mogelijk te werken 
+                      <br>Om onze zaak open te houden in deze crisis.</br>
+                      <br>Doet u mee?</br></center>                 
+  </div>
+</div>
+</div>
+  <div class="container">
+  <div class="row row-cols-4">
+    <div class="col"><div class="card" style="width: 18rem; margin-top: 30; ">
+  <img src="./img/checkapp.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text">Wij zijn verplicht om u coronabewijs te controleren.</p>
+  </div>
+</div></div>
+    <div class="col"><div class="card" style="width: 18rem; margin-top: 30;">
+  <img src="./img/handen.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text">Graag verzoeken wij u om bij intree u handen te indefecteren.</p>
+  </div>
+</div></div>
+    <div class="col"><div class="card" style="width: 17rem; margin-top: 30;">
+  <img src="./img/elleboog.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text">Bij niezen, graag in u elleboog niesen</p>
+  </div>
+</div></div>
+    <div class="col"><div class="card" style="width: 18rem; margin-top: 30">
+  <img src="klachten.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text">Bij klachten, verzoeken wij u om thuis te blijven.</p>
+  </div>
+</div></div>
+  </div>
+</div>
+</center>
+</div>
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
