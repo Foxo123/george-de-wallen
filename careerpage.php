3ef3c309bd366b90377ce2372efedf66f9b340bc
Mahircan Alkan <328827@student.mboutrecht.nl>
2021-10-11 14:13:04 +0200

    2nd career page commit
    styling and personel added

---
+++ careerpage.php
@@ -0,0 +1,67 @@
+<link rel="stylesheet" href="./css/addcareerpage.css">
+
+<div class="loader" id="loader">
+    <div class="text-loading" id="preloader">
+        <h3><span>George</span> De Wallen</h3>
+    </div>
+</div>`
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel="icon" type="image/png" sizes="32x32" href="./images/favicon-32x32.png">
+
+<body id="bodycareer">
+  <div class="header" id="headercareer">
+    <h1>Our career page!</h1>
+</head>
+
+<div class="container" id="containercareer2">
+  <div class="card2">
+    <div class="card-header">
+      <img src="./img/career1.jpg" alt="rover" />
+    </div>
+    <div class="card-body1">
+      <span class="tag1 tag-teal1">Kitchen</span>
+      <h4>
+        Mike Dolly's experience!
+      </h4>
+      <p>   
+        In my beginning years in the late 90's, vegan food wasn't that populair but now after a lot of experience and skill i can show u the world of vegan!
+      </p>
+      <div class="user">
+        <div class="user-info">
+          <h5>Mike Dolly</h5>
+          <small>Head-chef
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="card2">
+    <div class="card-header">
+      <img src="./img/career2.jph.jpg" alt="rover" />
+    </div>
+    <div class="card-body1">
+      <span class="tag1 tag-teal1">Kitchen</span>
+      <h4>
+        Kian Aliters experience!
+      </h4>
+      <p>   
+        In my beginning years in the late 90's, vegan food wasn't that populair but now after a lot of experience and skill i can show u the world of vegan!
+      </p>
+      <div class="user">
+        <div class="user-info">
+          <h5>Kian Aliter</h5>
+          <small>Floor-waiter
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  
