beef7ecd9f79c1dc9714ac106cad87bcbad49c08
Mahircan Alkan <328827@student.mboutrecht.nl>
2021-10-25 14:59:13 +0200

    sprint 2 careerpage style
    styling ad requirements added to the page

--- careerpage.php
+++ careerpage.php
@@ -34,8 +34,9 @@
 </div>
 
 
+<img src="./img/georgelogo1.jpg" alt="">
 
-<h1 style="font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; color:black">Top reviews!</h1>
+<h1 style="padding-top: 75px; font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; color:black">Top reviews!</h1>
 
 <section class="py-5">
     <div class="container">
@@ -43,9 +44,9 @@
             <div class="col-4 mx-auto">
                 <blockquote class="blockquote blockquote-custom bg-white p-5 shadow rounded">
                     <div class="blockquote-custom-icon bg-info shadow-sm"><i class="fa fa-quote-left text-white"></i></div>
-                    <p class="mb-0 mt-2 font-italic">Amazing foods and delicious desserts! Kanye enjoyed the fish and the personel were very kind! Thanks and see u later!<a href="#" class="text-info">@deliciousss</a>."</p>
+                    <p class="mb-0 mt-2 font-italic">Killing it here in George restaurant! nice dinner and good foods! I will be back!<a href="#" class="text-info">@deliciousss</a>."</p>
                     <footer class="blockquote-footer pt-3 mt-3 border-top">twitter
-                        <cite title="Source Title">Kim Kardashian</cite>
+                        <cite title="Source Title">Brad< Pitt</cite>
                     </footer>
                 </blockquote>
                 <!-- END -->
@@ -63,9 +64,9 @@
             <div class="col-4 mx-auto" style="border-left-style: solid;">
                 <blockquote class="blockquote blockquote-custom bg-white p-5 shadow rounded">
                     <div class="blockquote-custom-icon bg-info shadow-sm"><i class="fa fa-quote-left text-white"></i></div>
-                    <p class="mb-0 mt-2 font-italic">Amazing foods and delicious desserts! Kanye enjoyed the fish and the personel were very kind! Thanks and see u later!<a href="#" class="text-info">@deliciousss</a>."</p>
+                    <p class="mb-0 mt-2 font-italic">WOW! I loved my meal here and the people were super nice! They had a great menu choice and the best lasagne in the city! go here!<a href="#" class="text-info">@deliciousss</a>."</p>
                     <footer class="blockquote-footer pt-3 mt-3 border-top">twitter
-                        <cite title="Source Title">Kim Kardashian</cite>
+                        <cite title="Source Title">Michael B Jordan</cite>
                     </footer>
                 </blockquote>
                 <!-- END -->
@@ -76,8 +77,15 @@
 
 <div class="divider"></div>
 
-<div class="">
-  hoi
+<div class="container" style="font-size: 50px;">
+  <p id="p1">
+    In order to work here you should....
+       <ins>be at least above 16,  </ins>
+       <ins>have a good motivation and </ins>
+       <ins>are willing to work overtime!</ins>
+       contact us in our contact page!
+       Send us an email and we will contact you!
+  </p>
 </div>
 
 
@@ -87,7 +95,7 @@
     <!-- Team Member 1 -->
     <div class="col-xl-3 col-md-6 mb-4">
       <div class="card border-0 shadow">
-        <img src="https://source.unsplash.com/TMgQMXoglsM/500x350" class="card-img-top" alt="...">
+        <img src="./img/chefcareerjpg.jpg" class="card-img-top" alt="...">
         <div class="card-body text-center">
           <h5 class="card-title mb-0">Chef cook!</h5>
           <div class="card-text text-black-50">Vegan specialist</div>
@@ -97,7 +105,7 @@
     <!-- Team Member 2 -->
     <div class="col-xl-3 col-md-6 mb-4" style="border-left-style: solid;">
       <div class="card border-0 shadow">
-        <img src="https://source.unsplash.com/9UVmlIb0wJU/500x350" class="card-img-top" alt="...">
+        <img src="./img/staffcareer.jpg" class="card-img-top" alt="...">
         <div class="card-body text-center">
           <h5 class="card-title mb-0">Dessert artist!</h5>
           <div class="card-text text-black-50">Less is more(ingredients)</div>
@@ -107,17 +115,18 @@
     <!-- Team Member 3 -->
     <div class="col-xl-3 col-md-6 mb-4" style="border-left-style: solid;">
       <div class="card border-0 shadow">
-        <img src="https://source.unsplash.com/sNut2MqSmds/500x350" class="card-img-top" alt="...">
+        <img src="./img/dessercareer.jpg" class="card-img-top" alt="...">
         <div class="card-body text-center">
           <h5 class="card-title mb-0">Staff member!</h5>
           <div class="card-text text-black-50">Happy to see you!</div>
         </div>
       </div>
+      <img src="georgelogo1.jpg" alt="">
     </div>
     <!-- Team Member 4 -->
     <div class="col-xl-3 col-md-6 mb-4" style="border-left-style: solid;">
       <div class="card border-0 shadow">
-        <img src="https://source.unsplash.com/7u5mwbu7qLg/500x350" class="card-img-top" alt="...">
+        <img src="./img/bartendercareer.jpg" class="card-img-top" alt="...">
         <div class="card-body text-center">
           <h5 class="card-title mb-0">Bartender!</h5>
           <div class="card-text text-black-50">Drinks with more taste</div>
