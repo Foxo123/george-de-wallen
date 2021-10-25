d2f2e543416a17a3b413eecceef8e91a5eecb414
Kapello <330990@student.mboutrecht.nl>
2021-10-25 16:06:35 +0200

    Added a gallery page
    And a carousel to complete the user story: Add a gallery

--- gallery.php
+++ gallery.php
@@ -8,5 +8,129 @@
 </head>
 <body>
     
+                <div class="row" style="padding-top: 175px;">
+    <div class="col-4" style="padding:30px;">
+            <img  class="center-cropped" src="./img/george1.jpg"
+             class=" rounded" style=" width: 100%;  margin: auto;border: 3px solid light;">
+    </div>
+    <div class="col-4" style="padding:30px;">
+             <img  class="center-cropped" src="./img/george2.jpg" 
+             class=" rounded" style=" width: 100%;  margin: auto;border: 3px solid light;">
+    </div>
+    <div class="col-4" style="padding:30px;">
+             <img  class="center-cropped" src="./img/george3.jpg" 
+             class=" rounded" style=" width: 100%;  margin: auto;border: 3px solid light;">
+    </div>
+    <div class="col-4" style="padding:30px;">
+            <img  class="center-cropped2" src="./img/george4.jpg"
+             class=" rounded" style=" width: 100%;  margin: auto;border: 3px solid light;">
+    </div>
+    <div class="col-4" style="padding:30px;">
+            <img  class="center-cropped" src="./img/george5.jpg"
+             class=" rounded" style=" width: 100%;border: 3px solid light;">
+    </div>
+    <div class="col-4" style="padding:30px;">
+            <img  class="center-cropped" src="./img/vegan.jpg"
+             class=" rounded" style=" width: 100%;  margin: auto;border: 3px solid light;">
+    </div>
+    <div class="col-4" style="padding:30px;">
+            <img  class="center-cropped" src="./img/bar.jpg"
+             class=" rounded" style=" width: 100%;  margin: auto;border: 3px solid light;">
+    </div>
+    <div class="col-4" style="padding:30px;">
+            <img  class="center-cropped" src="./img/bench.jpg"
+             class=" rounded" style=" width: 100%;  margin: auto;border: 3px solid light;">
+    </div>
+    <div class="col-4" style="padding:30px;">
+            <img  class="center-cropped" src="./img/mood.jpg"
+             class=" rounded" style=" width: 100%;  margin: auto;border: 3px solid light;">
+    </div>
+    
+                    </div>
+                    
+
+                    <!-- Carousel  -->
+                   <center> <div style="width:900px;">
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="7"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="8"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="9"></li>
+
+  </ol>
+  <div class="carousel-inner" style="width:900px">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="./img/george1.jpg" alt="First slide">
+      <div class="carousel-caption d-none d-md-block"> <h4 style="color:white;size:12px;style:bold;text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;">Our great service speaks for itself!</h4></div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="./img/george2.jpg" alt="Second slide">
+      <div class="carousel-caption d-none d-md-block"><h4 style="color:white;size:12px;style:bold;text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;">A fully stacked bar is waiting for your arrival!</h4></div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="./img/george3.jpg" alt="Third slide">
+      <div class="carousel-caption d-none d-md-block">  <h4 style="color:white;size:12px;style:bold;text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;">The greatest chefs are working with care</h4></div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="./img/george4.jpg" alt="Fourth slide">
+      <div class="carousel-caption d-none d-md-block"> <h4 style="color:white;size:12px;style:bold;text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;">Fresh seafood from the shore!</h4></div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="./img/george5.jpg" alt="Fifth slide">
+      <div class="carousel-caption d-none d-md-block">  <h4 style="color:white;size:12px;style:bold;text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;">The best sushi a la carte</h4></div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="./img/7.jpg" alt="Sixth slide">
+      <div class="carousel-caption d-none d-md-block">  <h4 style="color:white;size:12px;style:bold;text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;">Check out our new location!</h4></div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="./img/mood.jpg" alt="Seventh slide">
+      <div class="carousel-caption d-none d-md-block">  <h4 style="color:white;size:12px;style:bold;text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;">Fully renovated interior!</h4></div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="./img/bar.jpg" alt="Eight slide">
+      <div class="carousel-caption d-none d-md-block">  <h4 style="color:white;size:12px;style:bold;text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;">A great selection of wines aged and bottled.</h4></div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="./img/bench.jpg" alt="Ninth slide">
+      <div class="carousel-caption d-none d-md-block">  <h4 style="color:white;size:12px;style:bold;text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;">Comfort for any time of the day!</h4></div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="./img/kadefoto1.jpg" alt="Tenth slide">
+      <div class="carousel-caption d-none d-md-block">  <h4 style="color:white;size:12px;style:bold;text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;">From Amsterdammers for Amsterdammers!</h4></div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="false"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="false"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+</div>
+</center>
+                    <!-- Carousel End -->
+        <style>.center-cropped {
+  object-fit: none; /* Do not scale the image */
+  object-position: center; /* Center the image within the element */
+  height: 200px;
+  width: 175px;
+}
+.center-cropped2 {
+  object-fit: none; /* Do not scale the image */
+  object-position:bottom; /* Center the image within the element */
+  height: 175px;
+  width: 120px;
+}</style>
+<link rel="stylesheet" href="gallery.css">
 </body>
 </html>
