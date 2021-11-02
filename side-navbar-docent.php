e3b5fa82ebd5965f1e306fbcdc3d640e65303ae2
LuucStigter <stigterluuc@gmail.com>
2021-11-02 16:28:50 +0100

    added all pages + working side navbar
    added all pages + working side navbar

---
+++ side-navbar-docent.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Side Navigation Bar</title>
+    <link rel="stylesheet" href="./css/side-navbar-docent.css">
+    <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
+</head>
+
+<body>
+
+    <div class="wrapper">
+        <div class="sidebar">
+            <h2>Sidebar</h2>
+            <ul>
+                <li><a href="./docent-home.php"><i class="fas fa-home"></i>Home</a></li>
+                <li><a href="./schedule.php"><i class="fas fa-list"></i>Schedule</a></li>
+                <li><a href="./result.php"><i class="fas fa-chart-bar"></i>Results</a></li>
+                <li><a href="./docent-message.php"><i class="fas fa-envelope"></i>message</a></li>
+            </ul>
+        </div>
+    </div>
+
+</body>
+
+</html>
