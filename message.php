e89b40273e58b7d613e9f47789859cd8abd8c9ae
Tom Lengkeek <330713@student.mboutrecht.nl>
2021-11-05 09:16:18 +0100

    added message
    added message wrongactivation page

--- message.php
+++ message.php
@@ -153,7 +153,12 @@
             </div>';
             header("Refresh: 3; ./index.php?content=reservation");
     break;
-
+    case "wrongactivationpage" :
+      echo '<div class="alert alert-danger mt-5 w-50 mx-auto text-center" role="alert">
+              You are on the wrong activation page, please try again
+            </div>';
+            header("Refresh: 3; ./index.php?content=login");
+    break;
     default:
       header("Location: ./index.php?content=home");
     break;
