b73d8a3e47ceab6bbb84ab7441eed956fa37ed52
Tom Lengkeek <330713@student.mboutrecht.nl>
2021-11-05 09:07:23 +0100

    updated acrivation
    there is now a seperate page for students because they need to fill in some more information
    
    activate_script.php now inputs a userrole into the database
    
    also changed styling a little

--- activate.php
+++ activate.php
@@ -1,54 +1,70 @@
 <?php
+
+
 if (!(isset($_GET["content"]) && isset($_GET["em"]) && isset($_GET["pwh"]))) {
   header("Location: ./index.php?content=message&alert=hacker-alert");
 }
+
+include("./connect_db.php");
+include("./functions.php");
+
+$email = sanitize($_GET["em"]);
+
+if (!check_userrole($email, array('klant', 'eigenaar', 'docent', 'begeleider')) )
+{
+  header("Location: ./index.php?content=message&alert=wrongactivationpage");
+  exit();
+} 
+else {
 ?>
+
 
 <div class="container my-5">
   <div class="row">
     <div class="col-12 col-sm-6">
       <form action="./index.php?content=activate_script" method="post">
         <div class="form-group">
-          <label for="inputPassword">Kies een nieuw wachtwoord:</label>
+          <label for="inputPassword">password:</label>
           <input name="password" type="password" class="form-control" id="inputPassword" aria-describedby="passwordHelp" autofocus>
-          <small id="passwordHelp" class="form-text text-muted">Kies een veilig wachtwoord..</small>
+          <small id="passwordHelp" class="form-text text-muted">choose a safe password..</small>
         </div>
         <div class="form-group">
-          <label for="inputPasswordCheck">Type het wachtwoord opnieuw:</label>
+          <label for="inputPasswordCheck">password confirmation:</label>
           <input name="passwordCheck" type="password" class="form-control" id="inputPasswordCheck" aria-describedby="passwordHelpCheck">
           <small id="passwordHelpCheck" class="form-text text-muted">Ter controle voert u nogmaals uw wachtwoord in...</small>
         </div>
         <div class="row">
           <div class="col-4">
             <div class="form-group">
-              <label for="inputFirstName">Voornaam:</label>
+              <label for="inputFirstName">firstname:</label>
               <input name="firstName" type="text" class="form-control" id="inputFirstName" aria-describedby="firstNameHelp">
             </div>
           </div>
           <div class="col-4">
             <div class="form-group">
-              <label for="inputInfix">Tussenvoegsel:</label>
+              <label for="inputInfix">infix:</label>
               <input name="infix" type="text" class="form-control" id="inputInfix" aria-describedby="InfixHelp">
             </div>
           </div>
           <div class="col-4">
             <div class="form-group">
-              <label for="inputLastName">Achternaam:</label>
+              <label for="inputLastName">last name:</label>
               <input name="lastName" type="text" class="form-control" id="inputLastName" aria-describedby="lastNameHelp">
             </div>
           </div>
         </div>
         <div class="form-group">
-          <label for="inputPhoneNumber">telefoon nummer:</label>
+          <label for="inputPhoneNumber">phone number:</label>
           <input name="phoneNumber" type="tel" class="form-control" id="inputPhoneNumber" aria-describedby="phoneNumberHelp" pattern="06[0-9]{8}" placeholder="0612345678">
         </div>
         <input type="hidden" name="em" value="<?php echo $_GET["em"]; ?>">
         <input type="hidden" name="pwh" value="<?php echo $_GET["pwh"]; ?>">
-        <button type="submit" class="btn btn-success btn-lg btn-block">Activeer</button>
+        <button type="submit" class="btn btn-secondary btn-lg btn-block">Activeer</button>
       </form>
     </div>
-    <div class="col-12 col-sm-6">
-      <img src="./img/perzik.jpg" alt="perzik" class="w-75 mx-auto d-block">
+    <div class="col-12 col-sm-6" style=" border-left: 2px solid;">
+      <img src="./img/dewallen3.jpg" alt="perzik" class="w-100 mx-auto d-block">
     </div>
   </div>
 </div>
+<?php } ?>
